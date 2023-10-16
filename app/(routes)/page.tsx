86d813a803e34ffc4d22234d01029e49fec42456
Emir Risyad <emirrsyd@gmail.com>
2023-10-16 22:59:41 +0200

    added featured product page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,24 @@
+import getBillboard from '@/actions/getBillboard';
+import getProducts from '@/actions/getProducts';
 import { Billboard } from '@/components/Billboard';
+import { ProductList } from '@/components/ProductList';
 import { Container } from '@/components/ui/Container';
 
 export const revalidate = 0;
 
-const HomePage = () => {
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard('asas');
   return (
     <Container>
       <div className='space-y-10 pb-10'>
-        <Billboard />
+        <Billboard data={billboard} />
+      </div>
+      <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+        <ProductList
+          title='Featured Products'
+          items={products}
+        />
       </div>
     </Container>
   );
