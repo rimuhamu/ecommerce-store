51993c2cd3885192843acb2d956e5ebfe3b436e9
Emir Risyad <emirrsyd@gmail.com>
2023-10-18 21:54:42 +0200

    added individual category screen

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -12,7 +12,8 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`,
           className
         )}
-        ref={ref}>
+        ref={ref}
+        {...props}>
         {children}
       </button>
     );
